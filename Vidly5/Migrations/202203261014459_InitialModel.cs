c8f4b8f77a0d5013acf8867c935cefdb81b55509
omkar-narhe <omkarnarhe2212@gmail.com>
2022-03-27 10:34:50 +0530

    First Migration and DB Created

---
+++ Vidly5/Migrations/202203261014459_InitialModel.cs
@@ -0,0 +1,36 @@
+﻿namespace Vidly5.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+            DropTable("dbo.Customers");
+            DropTable("dbo.Movies");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+    }
+}
---
+++ Vidly5/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly5.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        
+        public ApplicationDbContext()
+        {
+        }   
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
